87ad278f6bbedc34a57dcf8ab0450ddd5497bae6
David Winings <wisp558@gmail.com>
2014-02-12 18:46:20 -0500

    Added logging files.

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject clojurver "1.0.0-SNAPSHOT"
   :description "A simple webserver written in Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [server-socket "1.0.0"]]
-  :main clojurver.core/-main)
+                 [server-socket "1.0.0"]
+                 [log4j "1.2.5"]
+                 [org.clojure/tools.logging "0.2.6"]]
+  :main clojurver.core/-main
+  :source-paths ["test"]
+  :test-paths ["src"])
